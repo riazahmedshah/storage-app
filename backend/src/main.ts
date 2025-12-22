3bd0a7ffc09baddb8c75d898fb581ae3a4b03c6f
riazahmedshah <riyazsh360@gmail.com>
2025-12-23 00:58:39 +0530

    added: create dir api

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,5 +1,5 @@
 import express, { Request } from "express";
-import { open, opendir, rename } from "node:fs/promises";
+import { mkdir, open, opendir, rename } from "node:fs/promises";
 import path from "node:path";
 const app = express();
 const PORT =  4000;
@@ -51,11 +51,12 @@ app.post("/trash/:filename", async(req, res) => {
   }
 });
 
-app.post("/createFile", async(req, res) => {
+// TODO: handle proper paths...
+app.post("/new-file{/*filename}", async(req, res) => {
   const filename = req.header("filename");
   if(!filename) return res.status(404).json({msg:"Filename is missing"});
   try {
-    const fileHandle = await open(filename, 'w');
+    const fileHandle = await open(`${import.meta.dirname}/public/${filename}`, 'w');
 
     const writeStream = fileHandle.createWriteStream();
     res.pipe(writeStream);
@@ -67,15 +68,45 @@ app.post("/createFile", async(req, res) => {
 
     writeStream.on('finish', () => {
       console.log(`File: ${filename} Uploaded successfully`);
-    })
-  } catch (error) {
-    
+    });
+
+    res.status(200).json({msg:`File ${filename} created successfully`});
+  } catch (error:any) {
+    if(error.code == 'ENOENT'){
+      res.status(404).json({msg:`File ${filename} not found`});
+    }else{
+      console.error(error);
+      res.status(500).json({ msg: "An internal error occurred" });
+    }
+  }
+});
+
+app.post("/new-folder{/*folderpath}", async(req:Request<{folderpath: string[]}>, res) => {
+  const { folderpath } = req.params;
+  const fullpath = folderpath?.join("/");
+  const targetPath = path.join(`${import.meta.dirname}/public/`, fullpath ?? "");
+
+  try {
+    await mkdir(targetPath, {recursive:false});
+    res.status(200).json({msg:"Directory created."})
+  } catch (error:any) {
+    if(error.code === 'EEXIST'){
+      res.status(409).json({msg:`Directory ${fullpath} Already Exists`})
+    }else{
+      console.error(error);
+      res.status(500).json({msg:"Error in Dir API"});
+    }
   }
 })
 
-app.patch("/rename/:filename", async(req, res) => {
+app.patch("/rename{/*filename}", async(req:Request<{filename: string[]}>, res) => {
   const {filename} = req.params
   const {newFileName} = req.body;
+
+  const dirPath = filename.join("/");
+
+  console.log(dirPath);
+  
 
   try {
     await rename(
