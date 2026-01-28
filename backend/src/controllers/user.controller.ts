b5c74e2011c2737d4c896f68aa01f39d664eed41
riazahmedshah <riyazsh360@gmail.com>
2026-01-28 23:00:57 +0530

    signed cookies

--- backend/src/controllers/user.controller.ts
+++ backend/src/controllers/user.controller.ts
@@ -1,15 +1,17 @@
+import { Buffer } from "node:buffer";
+
 import mongoose from "mongoose";
 import { NextFunction, Request, Response } from "express";
 
 import { User } from "../models/user.model.js";
 import { Directory } from "../models/directory.model.js";
 import { AppError } from "../utils/AppError.js";
-import { error } from "node:console";
+import { createHash } from "node:crypto";
 
 export const createUser = async (
-  req:Request,
-  res:Response,
-  next:NextFunction
+  req: Request,
+  res: Response,
+  next: NextFunction,
 ) => {
   const { name, email, password } = req.body;
   const session = await mongoose.startSession();
@@ -31,60 +33,67 @@ export const createUser = async (
       rootDirId,
     });
 
-  await newUser.save({ session });
+    await newUser.save({ session });
 
-  const newDir = new Directory({
-    _id: rootDirId,
-    name: `root-${email}`,
-    userId,
-    parentDirId: null
-  });
+    const newDir = new Directory({
+      _id: rootDirId,
+      name: `root-${email}`,
+      userId,
+      parentDirId: null,
+    });
 
-  await newDir.save({ session });
+    await newDir.save({ session });
 
     await session.commitTransaction();
     res.status(201).json({ msg: "User Created successfully" });
-  } catch (error:any) {
-    if(session.inTransaction()){
+  } catch (error: any) {
+    if (session.inTransaction()) {
       await session.abortTransaction();
     }
     next(error);
   } finally {
     await session.endSession();
   }
-}
+};
 
 export const login = async (
-  req:Request,
-  res:Response,
-  next:NextFunction
+  req: Request,
+  res: Response,
+  next: NextFunction,
 ) => {
   const { email, password } = req.body;
   try {
     const user = await User.findOne({ email });
-    if (!user || user.password !== password){
+    if (!user || user.password !== password) {
       throw new AppError("Invalid credentials", 401);
     }
     const { password: _p, ...userSafe } = user.toObject();
-    res.cookie("uid", user._id.toString(), {
-      maxAge: 60 * 60 * 1000,
+    const cookiePayload = {
+      id: user._id,
+      expiry: Math.round(Date.now() / 1000 + 60).toString(16),
+    };
+
+    const singedCookie = createHash("sha256")
+      .update(JSON.stringify(cookiePayload))
+      .update(process.env.SECRET!)
+      .digest('base64url');
+
+    const base64Cookie = Buffer.from(JSON.stringify(cookiePayload)).toString(
+      "base64url",
+    );
+    res.cookie("token", `${base64Cookie}.${singedCookie}`, {
+      maxAge: 60 * 60 * 1000 * 7 * 24,
     });
     res.status(200).json({ userSafe });
-  } catch(error) {
-    next(error)
+  } catch (error) {
+    next(error);
   }
-}
+};
 
-export const getUser = async (
-  req:Request,
-  res:Response
-) => {
+export const getUser = async (req: Request, res: Response) => {
   const { name, email } = req.user;
   res.status(200).json({ name, email });
-}
-export const logout = async (
-  req:Request,
-  res:Response
-) => {
+};
+export const logout = async (req: Request, res: Response) => {
   res.clearCookie("uid");
-}
+};
--- backend/src/middleware/auth.ts
+++ backend/src/middleware/auth.ts
@@ -1,20 +1,48 @@
+import { Buffer } from "node:buffer";
 import { NextFunction, Request, Response } from "express";
 
-import { ObjectId } from "mongodb";
 import { User } from "../models/user.model.js";
+import { createHash, timingSafeEqual } from "node:crypto";
 
-
-export async function authMiddleware(req: Request, res: Response, next: NextFunction) {
-  const { uid } = req.cookies;
-  if (!uid) {
+export async function authMiddleware(
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) {
+  const { token } = req.cookies;
+  if (!token) {
     return res.status(401).json({ error: "No authentication cookie found" });
   }
+  const [base64Payload, signature] = token.split(".");
+  const cookiePayload = JSON.parse(
+    Buffer.from(base64Payload, "base64url").toString(),
+  );
 
-  const isUserExists = await User.findOne({_id: new ObjectId(uid)});
+  // console.log(cookiePayload, signature);
+  const verifyCookie = createHash("sha256")
+    .update(JSON.stringify(cookiePayload))
+    .update(process.env.SECRET!)
+    .digest('base64url');
+
+  if(verifyCookie !== signature){
+    return res.status(401).json({ error: "Bad Cookie, chala ja BSDK..." });
+  }
+
+  // return res.send("Okkk");
+
+  // console.log(id, expiry);
+  if (new Date() > new Date(parseInt(cookiePayload.expiry, 16) * 1000)) {
+    res.clearCookie("token");
+    return res.status(401).json({
+      success: false,
+      message: "Token is expired",
+    });
+  }
+  const isUserExists = await User.findById(cookiePayload.id).lean();
 
   if (isUserExists) {
     req.user = isUserExists;
     return next();
-  } 
+  }
   return res.status(401).json({ error: "Unauthorised" });
 }
